3447943e988a741cd05ec3913cb277269b8c504e
Yuhesh Pandian P J <yuheshpandian@gmail.com>
2024-02-01 10:01:51 +0000

    update config

--- Boilerplate/config.py
+++ Boilerplate/config.py
@@ -1,4 +1,5 @@
 #The DEBUG IS SET TO TRUE SO YOU CAN DEVELOP YOUR SITE WITHOUT RELOADING FOR EVERY CHANGES
 DEBUG = True
 
-#make sure you edit this file before production or testing process
+
+# Make sure you edit this file before production or testing process
